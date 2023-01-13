d312e8a709036469aa42b48fab4693f073a5f4c6
fkry <fkrytm123@gmail.com>
2023-01-13 09:50:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#스프링 부트 프로젝트 설정
+1. 프로젝트 생성 링크 (https://start.spring.io)
+2. HELP.md -> READE.md로 변경 ->,gitignore에서 README.md 제거
+3. application.properties => application.yml로 변경
+4. yml 설정으로 톰캣 포트 변경하기
+5. devTools 설정
+-파일 ->설정 ->빌드,실행,배포 -> 컴파일러-> 프로젝트 자동빌드 체크
+-파일 -> 설정 -> 고급설정 -> 컴파일러 -> 개발된 애플리케이션~~auti make 체크
---
+++ src/main/java/com/example/apibasic/ApibasicApplication.java
@@ -0,0 +1,15 @@
+package com.example.apibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibasicApplication.class, args);
+		int a = 10;
+		SpringApplication.run(ApibasicApplication.class);
+	}
+
+}
